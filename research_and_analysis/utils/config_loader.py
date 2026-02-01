58900284d118191fb2ec77df6175f3036e8fe413
Bhagyashree Srinivasan <bhagyashree6599@gmail.com>
2026-02-01 12:26:43 +0000

    update .gitignore; fix import path in config_loader

--- research_and_analysis/utils/config_loader.py
+++ research_and_analysis/utils/config_loader.py
@@ -3,7 +3,7 @@ import yaml
 import json
 from pathlib import Path
 from logger import GLOBAL_LOGGER as log
-from exception.custom_exception import ResearchAnalystException
+from exceptions.custom_exception import ResearchAnalystException
 
 
 def _project_root() -> Path:
